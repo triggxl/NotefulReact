70ff40db47a1559a42dd0a3da95b8c7b70c83b6c
triggxl <triggxl@hotmail.com>
2021-03-09 20:24:17 -0500

    deploying to heroku...updating config file with PORT for production and local

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 
-server.listen(9090, () => {
-  console.log('Noteful json-server started at http://localhost:9090')
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 'http://localhost:9090';
+
+server.listen(PORT, () => {
+  console.log(`Noteful json server started at PORT: ${PORT}`)
 })
+
+// commit changes...push to heroku...heroku-server/notes if error check heroku logs
